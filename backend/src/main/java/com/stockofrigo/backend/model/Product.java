b4e723ccbbd341c6ee33880480db43d31b91ba51
Fantino <robin.fantino.rf@gmail.com>
2025-06-11 14:44:17 +0200

    I add product entity and the unit entity

--- backend/src/main/java/com/stockofrigo/backend/model/Product.java
+++ backend/src/main/java/com/stockofrigo/backend/model/Product.java
@@ -1,7 +1,5 @@
 package com.stockofrigo.backend.model;
 
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,100 +8,100 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "product")
 public class Product {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id", nullable = false, updatable = false)
+  private Long id;
 
-    @Column(name = "brand", length = 100, nullable = true)
-    private String brand;
+  @Column(name = "brand", length = 100, nullable = true)
+  private String brand;
 
-    @Column(name = "barcode", length = 20, unique = true, nullable = true)
-    private int barcode;
+  @Column(name = "barcode", length = 20, unique = true, nullable = true)
+  private int barcode;
 
-    @Column(name = "image_url", length = 500, nullable = true)
-    private String imageUrl;
+  @Column(name = "image_url", length = 500, nullable = true)
+  private String imageUrl;
 
-    @ManyToOne
-    @JoinColumn(name = "unit_id", nullable = true)
-    private Unit unitId;
+  @ManyToOne
+  @JoinColumn(name = "unit_id", nullable = true)
+  private Unit unitId;
 
-    @Column(name = "is_ingredient", nullable = false)
-    private boolean isIngredient;
+  @Column(name = "is_ingredient", nullable = false)
+  private boolean isIngredient;
 
-    @Column(name = "created_at", updatable = false, nullable = false)
-    private LocalDateTime createdAt;
+  @Column(name = "created_at", updatable = false, nullable = false)
+  private LocalDateTime createdAt;
 
-    @Column(name = "updated_at")
-    private LocalDateTime updatedAt;
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getBrand() {
-        return brand;
-    }
+  public String getBrand() {
+    return brand;
+  }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
 
-    public int getBarcode() {
-        return barcode;
-    }
+  public int getBarcode() {
+    return barcode;
+  }
 
-    public void setBarcode(int barcode) {
-        this.barcode = barcode;
-    }
+  public void setBarcode(int barcode) {
+    this.barcode = barcode;
+  }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
+  public String getImageUrl() {
+    return imageUrl;
+  }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
+  public void setImageUrl(String imageUrl) {
+    this.imageUrl = imageUrl;
+  }
 
-    public Unit getUnitId() {
-        return unitId;
-    }
+  public Unit getUnitId() {
+    return unitId;
+  }
 
-    public void setUnitId(Unit unitId) {
-        this.unitId = unitId;
-    }
+  public void setUnitId(Unit unitId) {
+    this.unitId = unitId;
+  }
 
-    public boolean isIngredient() {
-        return isIngredient;
-    }
+  public boolean isIngredient() {
+    return isIngredient;
+  }
 
-    public void setIngredient(boolean isIngredient) {
-        this.isIngredient = isIngredient;
-    }
+  public void setIngredient(boolean isIngredient) {
+    this.isIngredient = isIngredient;
+  }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
 
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
+  public void setCreatedAt(LocalDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
 
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
+  public LocalDateTime getUpdatedAt() {
+    return updatedAt;
+  }
 
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
+  public void setUpdatedAt(LocalDateTime updatedAt) {
+    this.updatedAt = updatedAt;
+  }
 }
