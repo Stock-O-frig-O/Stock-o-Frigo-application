fdf581250e7dc25eb3b955665ffa6f92ee926334
Fantino <robin.fantino.rf@gmail.com>
2025-06-11 14:36:57 +0200

    I add product entity and the unit entity

---
+++ backend/src/main/java/com/stockofrigo/backend/model/Unit.java
@@ -0,0 +1,21 @@
+package com.stockofrigo.backend.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "unit")
+public class Unit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @Column(name = "unit", nullable = false, length = 100)
+    private String unit;
+}
