43cb4f914f96eb9889c204e28163180c586a4995
Mahery <mahery.gonin@gmail.com>
2025-06-12 12:14:57 +0200

    [security](cors): Set allowed hosts for cors policy

--- backend/src/main/java/com/stockofrigo/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/stockofrigo/backend/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.stockofrigo.backend.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,14 +9,30 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Value("${cors.allowed-origin}")
+  private String allowedOrigin;
+
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors
+                    .configurationSource(request -> {
+                      CorsConfiguration config = new CorsConfiguration();
+                      config.setAllowedOrigins(List.of(allowedOrigin));
+                      config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+                      config.setAllowedHeaders(List.of("*"));
+                      config.setAllowCredentials(true);
+                      return config;
+                    })
+            )
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
     return http.build();
