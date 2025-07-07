97c2441ca9dfa761cbdbb5947218f8a22431b94a
Fantino <robin.fantino.rf@gmail.com>
2025-07-07 16:56:34 +0200

    add stock-page component and modify navigation route

---
+++ frontend/src/app/pages/stock-page/stock-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stock-page',
+  imports: [],
+  templateUrl: './stock-page.component.html',
+  styleUrl: './stock-page.component.scss'
+})
+export class StockPageComponent {
+
+}
