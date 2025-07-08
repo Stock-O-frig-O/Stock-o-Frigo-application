4d32e9da5c7baad419f2b1d36cfc7bdf75cad810
Mahery <mahery.gonin@gmail.com>
2025-07-08 10:48:47 +0200

    [FEAT](Auth): add Interceptor for send token in request (linted)

--- frontend/src/app/core/interceptors/auth.interceptor.ts
+++ frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,14 +1,12 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const router = inject(Router);
 
-  authService.verifyToken()
-  
+  authService.verifyToken();
+
   const token = authService.getToken();
 
   if (token) {
