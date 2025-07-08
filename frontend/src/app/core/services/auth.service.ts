fbf2fa4e196a445f81aba99d71715ae5bc75bff1
Mahery <mahery.gonin@gmail.com>
2025-07-08 10:40:02 +0200

    [FEAT](Auth): Extent auth service

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root',
@@ -9,6 +11,7 @@ import { environment } from '../../../environments/environment';
 export class AuthService {
   private apiUrl = environment.apiUrl;
   private http = inject(HttpClient);
+  private router = inject(Router);
 
   register(
     firstname: string,
@@ -24,10 +27,65 @@ export class AuthService {
   }
 
   login(email: string, password: string): Observable<string> {
-    return this.http.post(
-      `${this.apiUrl}/auth/login`,
-      { email, password },
-      { responseType: 'text' },
-    );
+    return this.http
+      .post(
+        `${this.apiUrl}/auth/login`,
+        { email, password },
+        { responseType: 'text' },
+      )
+      .pipe(
+        tap((token) => {
+          this.saveToken(token);
+        }),
+      );
+  }
+
+  logout() {
+    this.clearToken();
+    this.router.navigate(['/login']);
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  clearToken(): void {
+    localStorage.removeItem('token');
+  }
+
+  verifyToken(): void {
+    const token = this.getToken();
+    if (!token) return;
+
+    try {
+      const decodedToken = jwtDecode<{ exp: number }>(token);
+      const expiryDate = new Date(decodedToken.exp * 1000);
+      if (expiryDate < new Date()) {
+        this.clearToken();
+      }
+    } catch {
+      this.clearToken();
+    }
+  }
+
+  isLoggedIn(): boolean {
+    const token = this.getToken();
+    if (!token) return false;
+    try {
+      const decodedToken = jwtDecode<{ exp: number }>(token);
+      const expiryDate = new Date(decodedToken.exp * 1000);
+      if (expiryDate < new Date()) {
+        this.clearToken();
+        return false;
+      }
+      return true;
+    } catch {
+      this.clearToken();
+      return false;
+    }
   }
 }
