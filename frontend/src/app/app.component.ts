e78cb1110b8df50624fea4efcd9f2b579da40d52
Fantino <robin.fantino.rf@gmail.com>
2025-07-10 11:42:27 +0200

    changement de pc

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+// import Angular
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+// import components
 import { NavbarreComponent } from './components/navbarre/navbarre.component';
 import { LogoComponent } from './components/logo/logo.component';
+
+// import services
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +15,6 @@ import { LogoComponent } from './components/logo/logo.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  public authService: AuthService = inject(AuthService);
+}
